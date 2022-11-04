0c6c6a135a1a224a30468f6153feff6deb01934b
Przemyslaw <misiek440@gmail.com>
2022-11-04 11:44:34 +0100

    add style and array ingredients

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingridient } from './../shared/ingridient.model';
 
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
-  styleUrls: ['./shoping-list.component.css']
+  styleUrls: ['./shoping-list.component.css'],
 })
 export class ShopingListComponent implements OnInit {
+  ingredients: Ingridient[] = [
+    new Ingridient('apples', 5),
+    new Ingridient('egss', 8),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
