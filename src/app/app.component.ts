867c10e293e81b0a2a4469838ecd88328eaa29f8
Przemyslaw <misiek440@gmail.com>
2022-11-08 16:23:58 +0100

    ngif learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes/recipe.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor() {}
+  recipeFlag: boolean = false;
+  shopingFlag: boolean = true;
   title = 'Recpie-shoplist';
+
+  recipeComponent(event: boolean) {
+    this.recipeFlag = event;
+    this.shopingFlag = false;
+  }
+  shopingComponent(event: boolean) {
+    this.shopingFlag = event;
+    this.recipeFlag = false;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() recipeComponent = new EventEmitter<boolean>();
+  @Output() shopingComponent = new EventEmitter<boolean>();
+  recipeFired(value: boolean) {
+    this.recipeComponent.emit(value);
+  }
+  shopingFired(value: boolean) {
+    this.shopingComponent.emit(value);
+  }
+}
